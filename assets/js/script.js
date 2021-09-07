7c26e535126e9c9dff8e9d398d1ecd1ef24ecf7d
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-07 18:27:10 -0400

    input validation, reset form after adding new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,17 @@ var taskFormHandler = function(event){
         type: taskTypeInput
     };
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        
+        alert("You need to fill our the task form!");
+        return false;
+    }
+    formEl.reset();
+
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+    
 }; 
     
 
